9cf6b9e2d1ff1d478461b9a3243806dd524297e9
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-09-25 22:41:51 +1000

    Update Item.m
    -Update comments to be more descriptive
    -Made initialisation function not require baseTR as input

--- Item.m
+++ Item.m
@@ -6,18 +6,26 @@ classdef Item < handle
 
     methods
         function self = Item(item,baseTr)
-        %Place item(.ply file) into environment at baseTR
-        self.obj = PlaceObject(item);
-        self.vertices = get(self.obj,'Vertices');
-        transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * baseTr.';
-        set(self.obj,'Vertices',transformedVertices(:,1:3));
+            %Place 'item' into environment at 'baseTR'
+            %'baseTR' is relative to global frame of reference
+            %item = '.ply'
+            %baseTr = 4x4 homogenous transformation
+            if nargin < 2
+                baseTr = eye(4);
+            end
+            hold on
+            self.obj = PlaceObject(item);
+            self.vertices = get(self.obj,'Vertices');
+            transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * baseTr.';
+            set(self.obj,'Vertices',transformedVertices(:,1:3));
         end
 
         function move(self,tr)
-        %Moves items relative to global frame of reference by tr
-
-        transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * tr.';
-        set(self.obj,'Vertices',transformedVertices(:,1:3));
+            %Moves items by 'tr' relative to global frame of reference
+            %tr = 4x4 homogenous transformation
+            hold on
+            transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * tr.';
+            set(self.obj,'Vertices',transformedVertices(:,1:3));
         end
     end 
 end 
