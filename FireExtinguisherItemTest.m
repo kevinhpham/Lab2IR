bf29434612042481627a2f7fdd951c7206285950
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-09-29 21:06:07 +1000

    Add Fire Extinguisher class
    Aswell as a test script for the Fire Extinguisher. You could use the Fire Extinguisher class as a template for other Items

---
+++ FireExtinguisherItemTest.m
@@ -0,0 +1,15 @@
+%To test 'Item' sub-classes. Creates a 5x5 grid of 'FireExtinguisher'
+%objects into the workspace
+close
+clear
+hold on
+camlight
+fire = cell(10,10);
+for j = 1:5
+    for i = 1:5
+        fire{j,i} = Fire_Extinguisher();
+        fire{j,i}.move(transl(i,j,0));
+    end
+end
+
+axis equal
