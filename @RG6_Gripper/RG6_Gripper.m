5e81ceb6f96102c2172e30690ff500350e1b83b6
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-01 00:34:12 +1000

    Create RG6_Gripper.m

---
+++ @RG6_Gripper/RG6_Gripper.m
@@ -0,0 +1,55 @@
+classdef RG6_Gripper <handle
+%Two finger gripper for E05 L
+%The Gripper robot is made up of two seperate serial link arms,
+% 'left' and 'right' representing fingers
+ properties(Access = public)              
+        left;
+        right;
+ end
+
+ methods
+     function self = RG6_Gripper(baseTr) 
+        self.CreateModel();
+        if nargin < 1			
+			baseTr = eye(4);				
+        end
+        self.left.base = self.left.base.T * baseTr;
+        self.right.base = self.right.base.T * baseTr * rpy2tr(pi, 0, 0, 'xyz');
+     end
+             
+     function CreateModel(self)   
+        links = [
+                Revolute('d', 0.152,'a',0.03,'alpha',pi/2,'qlim',[deg2rad(0),deg2rad(0)]) %create base of gripper;doesn't move or rotate
+                Revolute('d', 0,'a', 0.08, 'alpha', 0,'offset',deg2rad(35),'qlim',[deg2rad(0),deg2rad(60)])
+                Revolute('d', 0, 'a', 0.054, 'alpha',0,'offset',deg2rad(60),'qlim',[-deg2rad(60),deg2rad(0)])
+        ];
+        self.left =  SerialLink(links, 'name', 'Gripper_LEFT');
+        self.right = SerialLink(links, 'name', 'Gripper_RIGHT');
+     end
+
+    %Following function allow control of multiple links simultaneously
+     function moveBase(self,baseTr) 
+        self.left.base = self.left.base.T * baseTr;
+        self.right.base = self.right.base.T * baseTr;
+     end
+
+    function animateAll(self,q)
+        hold on
+        self.left.animate(q)
+        self.right.animate(q)
+    end
+
+    function plotAll(self,q)
+        hold on
+        self.left.plot(q)
+        self.right.plot(q)
+    end
+
+    function setDelay(self,t)
+        self.left.delay = t;
+        self.right.delay = t;
+    end
+
+    end
+end
+
