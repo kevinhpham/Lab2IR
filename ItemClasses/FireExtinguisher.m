857ab4f4a38562236a92b7b22df18abf78cbed2c
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-03 21:29:41 +1100

    Update and rename Fire_Extinguisher.m to FireExtinguisher.m

--- ItemClasses/FireExtinguisher.m
+++ ItemClasses/FireExtinguisher.m
@@ -1,13 +1,13 @@
 %Creates a 3d model of an ugly fire exinguisher
-classdef Fire_Extinguisher < Item
+classdef FireExtinguisher < Item
     properties
     end 
 
     methods
         %This function constructs an 'FireExtinguisher' objects which inherits
         %functions and data fields from 'Item' class.
-        function item = Fire_Extinguisher(baseTr) %change this to class name
-            plyName = 'Fire_Extinguisher.ply'; %change this to corresponding .ply file name
+        function item = FireExtinguisher(baseTr) %change this to class name
+            plyName = 'FireExtinguisher.ply'; %change this to corresponding .ply file name
             if nargin < 1
                 baseTr = eye(4);
             end
