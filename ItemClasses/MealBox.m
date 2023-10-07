c7d2fc22cd4e7842186f739c996267c110938081
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-07 18:35:08 +1100

    Update 'MealBox.m' to take flavour
    add an additional arguement after the the baseTR to choose flavour of mealbox

--- ItemClasses/MealBox.m
+++ ItemClasses/MealBox.m
@@ -4,10 +4,38 @@ classdef MealBox < Item
     end 
 
     methods
-        function item = MealBox(baseTr)
-            plyName = 'MealBox.PLY';
-            if nargin < 1
-                baseTr = eye(4);
+        function item = MealBox(baseTr,flavour)
+        %type is a string specifying which type of juice to create
+        %If no arguement provided use default values
+            if nargin <2
+                flavour = 'none';
+                if nargin < 1
+                    baseTr = eye(4);
+                end
+            end
+            
+            %Check what type of flavour has been requested and
+            %select appropriate Ply file
+            flavourL = lower(flavour);
+            switch(flavourL)
+                case 'veg'
+                    plyName = 'MealBoxVego.PLY';
+                case 'v'
+                    plyName = 'MealBoxVego.PLY';
+                case'vego'
+                    plyName = 'MealBoxVego.PLY';
+                case'vegetable'
+                    plyName = 'MealBoxVego.PLY';
+                case'vegetarian'
+                    plyName = 'MealBoxVego.PLY';
+                case'meat'
+                    plyName = 'MealBoxMeat.ply';
+                case'm'
+                    plyName = 'MealBoxMeat.ply';
+                case 'none'
+                    plyName = 'MealBox.ply';
+                otherwise
+                    plyName = 'MealBox.ply';
             end
             item = item@Item(plyName,baseTr)
         end
