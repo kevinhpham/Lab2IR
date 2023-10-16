3af97c2cb90de99ca8534d049c95ce8ae64c2a40
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-17 02:11:59 +1100

    Update WorkerRobotTest for new ply files

--- TestingFiles/WorkerRobotTest.m
+++ TestingFiles/WorkerRobotTest.m
@@ -1,11 +1,23 @@
 close
 hold on
 axis equal
-worker = E05_worker(transl(0,0,0));
-worker.robot.model.offset = [0 pi/2 -pi/3 0 0 0]
-rack = TrayStorage(transl(-0.4,0.6,-1.3));
-worker.plotWorkspace()
- tray = rack.addTrays(15);
- worker.pickup(tray(1))
- worker.pickup(tray(15))
-worker.robot.model.teach(worker.robot.model.getpos())
+worker(1) = E05_worker(transl(0,0,0));
+rack(1) = TrayStorage(transl(-0.4,0.6,0));
+trays1 = rack(1).addTrays(8);
+worker(1).plotWorkspace()
+worker(1).pickup(trays1(1))
+worker(1).pickup(trays1(8))
+
+
+%%Testing multi threading toolbox
+%worker2 = E05_L(transl(2,0,2));
+%worker2.model.plot(zeros(6))
+%rack(2) = TrayStorage(transl(-0.4,0.6,1));
+%trays2 = rack(2).addTrays(8);
+
+% parfor i = 1:2
+%     worker2.pickup(trays2(1))
+%     worker2.pickup(trays2(8))
+%     worker(1).pickup(trays1(1))
+%     worker(1).pickup(trays1(8))
+% end
