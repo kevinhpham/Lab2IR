b9fa35eb2fdcd2db36b94e0818812fec139b3ce5
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-18 01:57:28 +1100

    Update TestTrayStorage.m
    Update to work with latest ply models

--- TestingFiles/TestTrayStorage.m
+++ TestingFiles/TestTrayStorage.m
@@ -1,7 +1,7 @@
 %Test creating tray rack in random orientation and spawns in trays
 clear
 close
-basetr = transl(1,0,0)*rpy2tr(0,0,45,'deg');
+basetr = transl(1,0,0)*rpy2tr(0,0,0,'deg');
 traystorage = TrayStorage(basetr);
 trays = traystorage.addTrays(8);
 camlight
