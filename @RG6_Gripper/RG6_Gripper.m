44a1831d7cbe4c6bbc002b589de9dcdb6cadc923
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-20 07:14:07 +1100

    Update RG6_Gripper.m
    added additional functionality so programmers don't have to access individual fingers and can control gripper as a whole

--- @RG6_Gripper/RG6_Gripper.m
+++ @RG6_Gripper/RG6_Gripper.m
@@ -2,10 +2,10 @@ classdef RG6_Gripper <handle
 %Two finger gripper for E05 L
 %The Gripper robot is made up of two seperate serial link arms,
 % 'left' and 'right' representing fingers
- properties(Access = public)              
+ properties(Access = private)
         left;
         right;
-        height;
+        height; %current height of gripper
  end
 
  methods
@@ -16,11 +16,12 @@ classdef RG6_Gripper <handle
         end
         self.left.base = self.left.base.T * baseTr;
         self.right.base = self.right.base.T * baseTr * rpy2tr(pi, 0, 0, 'xyz');
-     end
+        tr = inv(self.left.base.T)*self.left.fkineUTS([0 0 0]);%transform of end effector in gripper base frame
+        self.height = tr(3,4);
+        end
              
      function CreateModel(self)   
         scale = 1; %changes size of the gripper
-        self.height = 0.2*scale;
         links = [
                 Revolute('d', 0.152*scale,'a',0.03*scale,'alpha',pi/2,'qlim',[deg2rad(0),deg2rad(0)]) %create base of gripper;doesn't move or rotate
                 Revolute('d', 0,'a', 0.08*scale, 'alpha', 0,'offset',deg2rad(35),'qlim',[deg2rad(0),deg2rad(60)])
@@ -30,19 +31,21 @@ classdef RG6_Gripper <handle
         self.right = SerialLink(links, 'name', 'Gripper_RIGHT');
      end
 
-    %Following function allow control of multiple links simultaneously
-     function moveBase(self,baseTr) 
-        self.left.base = self.left.base.T * baseTr;
-        self.right.base = self.right.base.T * baseTr;
+     function setBase(self,baseTr) 
+        self.left.base = baseTr;
+        self.right.base =baseTr*rpy2tr(0,0,pi);
      end
 
-    function animateAll(self,q)
+    function animate(self,q)
+        %animate gripper
         hold on
         self.left.animate(q)
         self.right.animate(q)
+        self.updateHeight();
     end
 
-    function plotAll(self,q)
+    function plot(self,q)
+        %plot both fingers of gripper
         hold on
         self.left.plot(q)
         self.right.plot(q)
@@ -53,6 +56,27 @@ classdef RG6_Gripper <handle
         self.right.delay = t;
     end
 
+    function [height]= getHeight(self)
+        %Returns heigh of gripper
+        height= self.height;
     end
+
+    function [q] = getPos(self)
+        %return joint positions of fingers,fingers have to be plotted first
+        q = self.left.getpos();
+    end
+
+    function [qLims] = getQlim(self)
+        qLims = self.left.qlim();
+    end
+ end
+
+ methods(Hidden)
+     function updateHeight(self)
+         %Updates height parameter, requires robot to be plotted
+         tr = inv(self.left.base.T)*self.left.fkineUTS(self.getPos());%transform of end effector in gripper base frame
+         self.height = tr(3,4);
+     end
+ end
 end
 
