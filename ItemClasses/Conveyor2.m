d27038bb9c4f9df6767e3dca7e98ff1dc1995e95
SimoneS14 <145756953+SimoneS14@users.noreply.github.com>
2023-10-22 15:51:01 +1100

    Added comments to Conveyor2
    Needs to be renamed to be without a number (I.e., Conveyor) to remain consistent with MATLAB Coding Standard for Industrial Robotics.

--- ItemClasses/Conveyor2.m
+++ ItemClasses/Conveyor2.m
@@ -1,11 +1,11 @@
-%Creates a movable 3d model of a Conveyor2
+% Creates a movable 3D model of c Conveyor 2
+% Conveyor 2 is the main conveyor used to assemble meal trays.
+
 classdef Conveyor2 < ConveyorClass
-    properties
-    end 
 
     methods
         function conveyor = Conveyor2(baseTr)
-            plyName = 'Conveyor2.0.PLY';
+            plyName = 'Conveyor2.0.PLY'; % Corresponds to the PLY file for the object
             if nargin < 1
                 baseTr = eye(4);
             end
