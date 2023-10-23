40c590b2482ddea2c4a911967abd51ca5a852ba0
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-24 00:52:33 +1100

    Update StockTrayTest.m
    Loads models of all small items

--- TestingFiles/StockTrayTest.m
+++ TestingFiles/StockTrayTest.m
@@ -1,6 +1,10 @@
 clear
 close
-l= StockTray()
 axis equal
 camlight
-l.addJuice(4,'c')
+item{1} = Cutlery(transl(0,0,0));
+item{2} = MealBox(item{1}.base*transl(-0.1,0,0),'m');
+item{3} = MealBox(item{2}.base*transl(-0.15,0,0),'v');
+item{4} = JuiceBox(item{3}.base*transl(-0.1,0,0),'b');
+item{5} = JuiceBox(item{4}.base*transl(-0.1,0,0),'o');
+item{6} = Tray(item{5}.base*transl(0,0,-0.05));
