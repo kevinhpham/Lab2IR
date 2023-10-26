fb1f5577a3ac7a98dab07193b5fb2c3f269fcf93
kevinhpham <145756916+kevinhpham@users.noreply.github.com>
2023-10-27 05:43:06 +1100

    Update Item.m
    added hitBox to "item" class which is a hitbox used for collision checking
    added new field "corners" which contains this upper and lower corners of the hitbox

--- ItemClasses/Item.m
+++ ItemClasses/Item.m
@@ -2,10 +2,11 @@
 classdef Item < handle
     properties
     obj;
-    vertices; %In local frame of item
-    base;
-    plyFile;
-    hitBox; %Corners of a rectangular prism that envelope the item's model. Corner points are relative to item's frame. Main use for basic collision detection
+    vertices;   %In local frame of item
+    base;       %4x4 transform matrix of item frame to parent frame
+    plyFile;    %Name of the plyfile of the item model
+    hitBox;     %Corners of a rectangular prism that envelope the item's model. Main use for basic collision detection.
+    corners;    %Corner points are relative to item's frame.
     end 
 
     methods
@@ -38,16 +39,18 @@ classdef Item < handle
             self.plyFile = item;                
             corner1 = [min(self.vertices(:,1)), min(self.vertices(:,2)), min(self.vertices(:,3))];  % A vector representing bottom corner of hitbox
             corner2 = [max(self.vertices(:,1)), max(self.vertices(:,2)), max(self.vertices(:,3))];  % A vector representing the opposite top corner of hitbox
-            self.hitBox = [corner1 ; corner2];    
+            self.corners = [corner1 ; corner2];
+            self.hitBox = Hitbox(corner1,corner2,self.base);
         end
 
         function move(self,tr)
-            %Moves items by 'tr' relative to global frame of reference
+            %Changes item's frame to 'tr'
             %tr = 4x4 homogenous transformation
             hold on
             transformedVertices = [self.vertices,ones(size(self.vertices,1),1)] * tr.';
             set(self.obj,'Vertices',transformedVertices(:,1:3));
             self.base = tr;
+            self.hitBox.update(self.corners(1,:),self.corners(2,:),tr);
         end
     end 
 end 
