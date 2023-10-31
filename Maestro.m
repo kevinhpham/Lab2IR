83b4c4012103fe1a0972720a534672191d1361a3
SimoneS14 <145756953+SimoneS14@users.noreply.github.com>
2023-11-01 00:11:23 +1100

    Update Maestro Inheritance
    Changed so inherit from Environment & handle.

--- Maestro.m
+++ Maestro.m
@@ -1,29 +1,29 @@
-classdef Maestro <handle
+classdef Maestro <Environment & handle
     %MAESTRO will be in charge of organising all the robots, environment
     %and conveyerbelts to work altogether
 
-    properties      % This sets up the constant properties of the workspace (static/non-static)
+    properties      % This sets up t he constant properties of the workspace (static/non-static)
                     % Comment out the nonStatic Environment as needed.
 
         % staticEnvironment = StaticEnvironment();
         % nonStaticEnvironment = NonStaticEnvironment();
-        environment = Environment(); % Both static and non static
+        % environment = Environment(); % Both static and non static
     end
     
     %% The master code that runs all the main robot tasks
     methods
         function self = Maestro()
-            self.CreateMeal();
+            self.TestFunction();
+            %self.SetupNonStaticWorkspace();
         end
 
-        function CreateMeal(self)
-            mealConveyor(1).setPushDistance(0.028*30/steps)    %distances set up to be inversely porpotional the number of steps in animation, so system will still work regardless of the number of steps of the animation
-            mealConveyor(1).setPushDistance(0.028*30/steps)    %distances set up to be inversely porpotional the number of steps in animation, so system will still work regardless of the number of steps of the animation
-            mealConveyor(2).setPushDistance(0.028*30/steps)
-            dobotConveyor(1).setPushDistance(0.032*30/steps)
-            dobotConveyor(2).setPushDistance(0.0317*30/steps)
-            dobotConveyor(3).setPushDistance(0.032*30/steps)
-            trayConveyor.setPushDistance(-0.029*30/steps) 
+        function TestFunction(self)
+            self.trayStorage;
+            self.chefPerson(1);
+
+            %self.chefPerson(1).setPushDistance(0.028*30/steps);
+            %This creates an error as it's not a method in the
+            %chefPersonClass
         end
         
     end
