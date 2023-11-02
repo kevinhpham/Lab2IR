20596d5e20687a6beedccbcc4ff2fd5c67d52834
SimoneS14 <145756953+SimoneS14@users.noreply.github.com>
2023-11-02 23:09:40 +1100

    Update UserDemo.m
    Prints out customised order to command window based on user input.
    Also prints out messages that would ideally be shown on a user interface:
    Press enter to view order.
    Order has been sent to kitchen

--- Demo/UserDemo.m
+++ Demo/UserDemo.m
@@ -1,43 +1,61 @@
 classdef UserDemo
+    % The purpose of this class is to demonstrate a user order interface 
+    % The function provides prompts to the user to create a custom meal
+    % After receiving inputs from the user, the function prints out the
+    % order to the command window
+
    properties
-      mealSelected;
-      juiceSelected;
+      mealResult;   % To store result: Vegetarian or Meat
+      juiceResult;  % To store result: Blackcurrent or Orange
    end
 
    methods
        function self = UserDemo()
-			    self.selectMeal();
-                self.selectJuice();
+           self.customiseMealTray()
        end
    end
 
    methods(Static)
-      function selectMeal()
+      function customiseMealTray()
+         % Select the Meal
          disp("Would you like a Vegetarian or Meat Meal?")
          mealChoice = input("Enter 'V' for Veg or 'M' for Meat ", "s");
-         %mealSelected = ['The meal you selected is ', mealChoice];
-         %disp(mealSelected)
-
-         if mealChoice == 'V' || mealChoice == 'M'
-             mealSelected = ['The meal you selected is ', mealChoice];
-             disp(mealSelected)
+         if mealChoice == 'V' || mealChoice == 'v'      % Input is not case sensitive
+             mealChoice = 'Vegetarian';                 % Convert letter to full word
+         elseif mealChoice == 'M'|| mealChoice == 'm'   
+             mealChoice = 'Meat';                       % Convert letter to full word
          else
-             disp('Input is not valid')
+             disp('Input is not valid') % Cannot input a number or character other than those specified.
          end
-      end
 
-      function selectJuice()
+         % Select the juice
          disp("Would you like a Blackcurrent or Orange Juice?")
          juiceChoice = input("Enter 'B' for Blackcurrent or 'O' for Orange ", "s");
-         %mealSelected = ['The meal you selected is ', mealChoice];
-         %disp(mealSelected)
 
-         if juiceChoice == 'B' || juiceChoice == 'O'
-             juiceSelected = ['The juice you selected is ', juiceChoice];
-             disp(juiceSelected)
+         if juiceChoice == 'B' || juiceChoice == 'b'    % Input is not case sensitive
+             juiceChoice = 'Blackcurrent';              % Convert letter to full word
+         elseif juiceChoice == 'o'|| juiceChoice == 'O'
+             juiceChoice = 'Orange';                    % Convert letter to full word
          else
-             disp('Input is not valid')
+             disp('Input is not valid') % Cannot input a number or character other than those specified.
          end
+         
+         disp(' ')
+         input("Press enter to display your custom meal", "s"); % Mimic user interface
+
+         % Display Customised Meal Result
+         disp(' ')
+         disp('Your Customised Meal Tray')
+         mealResult = ['Meal: ', mealChoice];
+         juiceResult = ['Juice: ', juiceChoice];
+         disp(mealResult)
+         disp(juiceResult)
+         disp('cutlery')
+
+         disp(' ')
+         input("Press enter to send order to kitchen", "s"); % Mimic user interface
+         disp(' ')
+         disp('Thank you! Your order has been sent to the kitchen.')
       end
 
    end
