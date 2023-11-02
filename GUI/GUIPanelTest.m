8226bfbad76f54ab67b2264da439acca82816079
emmapetraglia <emma.k.petraglia@student.uts.edu.au>
2023-11-02 23:26:57 +1100

    Update GUI
    GUI panel split into two panels. One for the Dobot Magician and One for the Elfin E05_L. Estop and release buttons have been completed on both panels and the changed callback functions for the q values have been added to both panels as well

--- GUI/GUIPanelTest.m
+++ GUI/GUIPanelTest.m
@@ -1,12 +1,6 @@
-%pendant = RobotsApp();
+pendant = RobotsApp();
 
 
-%robotE05L = E05_L();
-robotDM = DobotMagician();
-qE05L = zeros(1, 6);
-qDM = zeros(1,5);
-%q = robotE05L.getpos
 
-%qDM = robot.getpos
 
 
